da5efab1b067be407396386e1d79c0575c9e7006
EDUARDO <eduardogoe01@gmail.com>
2026-03-09 11:16:33 -0400

    Adicionar funções de soma, subtração, multiplicação e divisão
    Implementa funções básicas de uma calculadora simples.

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+// Mini projeto - Calculadora simples
+
+function somar(a, b) {
+  return a + b;
+}
+
+function subtrair(a, b) {
+  return a - b;
+}
+
+function multiplicar(a, b) {
+  return a * b;
+}
+
+function dividir(a, b) {
+  return a / b;
+}
+
+console.log("Soma:", somar(10, 5));
+console.log("Subtração:", subtrair(10, 5));
+console.log("Multiplicação:", multiplicar(10, 5));
+console.log("Divisão:", dividir(10, 5));
